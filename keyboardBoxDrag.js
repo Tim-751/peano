298b31b85bf97b324a20fdf6e31c52ea1f486004
Tim <tim-github@etkin.xyz>
2022-05-01 13:35:55 +0200

    Add playing with keyboard keys

--- keyboardBoxDrag.js
+++ keyboardBoxDrag.js
@@ -1,8 +1,7 @@
 keyboardBox.onmousedown = function (event) {
   // (1) prepare to moving: make absolute and on top by z-index
   keyboardBox.style.position = 'absolute'
-  keyboardBox.style.zIndex = 1000
-  let shiftX = event.clientX - keyboardBox.getBoundingClientRect().left
+  const shiftX = event.clientX - keyboardBox.getBoundingClientRect().left
 
   // move it out of any current parents directly into body
   // to make it positioned relative to the body
@@ -13,14 +12,14 @@ keyboardBox.onmousedown = function (event) {
     if (finalX < 0) {
       finalX = 0
     }
-    if (finalX > 1238 - 413) {
-      finalX = 1238 - 413
+    if (finalX > 1240 - 532) {
+      finalX = 1240 - 532
     }
     keyboardBox.style.left = finalX + 'px'
   }
 
-//   // move our absolutely positioned ball under the pointer
-//   moveAt(event.pageX, event.pageY)
+  //   // move our absolutely positioned ball under the pointer
+  moveAt(event.pageX)
 
   function onMouseMove (event) {
     moveAt(event.pageX, event.pageY)
@@ -31,13 +30,52 @@ keyboardBox.onmousedown = function (event) {
 
   // (3) drop the ball, remove unneeded handlers
   document.onmouseup = function () {
-    let Xpos = keyboardBox.getBoundingClientRect().left
-    let finalX = Math.round(Xpos/59)*59
+    const Xpos = keyboardBox.getBoundingClientRect().left
+    const finalX = Math.round(Xpos / 59) * 59
     keyboardBox.style.left = finalX + 'px'
+    keyboardBox.pos = Math.round(Xpos / 59)
     document.removeEventListener('mousemove', onMouseMove)
   }
 
   keyboardBox.ondragstart = function () {
     return false
+  }
+}
+
+function noteNumToNoteName (noteNum) {
+  octave = Math.floor(noteNum / 7) + 3
+
+  noteLetters = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+  sharps = [0, 'C#', 'D#', 0, 'F#', 'G#', 'A#', 0]
+  noteLetter = noteLetters[noteNum % 7]
+  return noteLetter + octave
+}
+
+keyboardBox.pos = 0
+noteLetters = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+sharps = [0, 'C#', 'D#', 0, 'F#', 'G#', 'A#']
+function playTonebyKeyOffset (keyOffset) {
+  // odd numbers are white keys. 1 is the leftmost white key in keyboardBox
+  // even numbers are black keys. 0 is the black key on the left border of keyboardBox
+  if (keyOffset % 2 === 1) {
+    noteLetter = noteLetters[Math.floor((keyOffset / 2) + keyboardBox.pos) % 7]
+  } else {
+    noteLetter = sharps[(Math.floor(keyOffset / 2) + keyboardBox.pos) % 7]
+    if (noteLetter === 0) {
+      return
+    }
+  }
+  octave = Math.floor((Math.floor(keyOffset / 2) + keyboardBox.pos) / 7) + 3
+
+  playTone(noteLetter + octave)
+}
+document.onkeydown = function (event) {
+  keys = ['q', 'a', 'w', 's', 'e', 'd', 'r', 'f', 't', 'g', 'y', 'h', 'u', 'j', 'i', 'k', 'o', 'l', 'p']
+  keyIndex = keys.findIndex(x => { if (x === event.key) return x })
+  if (event.key === 'ı') {
+    keyIndex = keys.findIndex(x => { if (x === 'i') return x })
+  }
+  if (keyIndex >= 0) {
+    playTonebyKeyOffset(keyIndex)
   }
 }
